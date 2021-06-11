1539cdc44923110137990a89616afbd8f1edc62e
ExaInsanity <nicolaiandorfer@outlook.com>
2021-06-11 12:23:23 +0200

    write the matcher

---
+++ HK-Matcher/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HK_Matcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Quit(Object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void LoadLastSession(Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadDefaultDir(Object sender, RoutedEventArgs e)
+        {
+            CompareToPath.Text = @"C:\Program Files\Steam\steamapps\common\Hollow Knight\hollow_knight_Data";
+        }
+
+        private void RunMatch(Object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Matcher matcher = new(CompareToPath.Text, CompareFromPath.Text, this);
+
+                matcher.Run();
+            }
+            catch(Exception ex)
+            {
+                Output.Text = $"Match failed: \n{ex}: {ex.Message}\n\n{ex.StackTrace}";
+            }
+        }
+    }
+}
---
+++ HK-Matcher/Matcher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HK_Matcher
+{
+    public class Matcher
+    {
+        private readonly String path1, path2;
+        private readonly MainWindow reference;
+        private readonly SHA512 provider;
+
+        public Matcher(String path1, String path2, MainWindow reference)
+        {
+            this.reference = reference;
+
+            if(!(path1 == null || path1.Trim() == ""))
+                this.path1 = path1;
+            else
+            {
+                reference.Output.Text = "Invalid first file path, please check your path. It must end in \"hollow_knight_Data\".";
+                return;
+            }
+
+            if(!(path2 == null || path2.Trim() == ""))
+                this.path2 = path2;
+            else
+            {
+                reference.Output.Text = "Invalid second file path, please check your path. It must end in \"hollow_knight_Data\".";
+                return;
+            }
+
+            provider = SHA512.Create();
+        }
+
+        public void Run()
+        {
+            Boolean matchFailed = false;
+            List<String> firstFiles = Directory.GetFiles(path1, "level*").ToList();
+            List<String> secondFiles = Directory.GetFiles(path2, "level*").ToList();
+
+            if(firstFiles.Count != secondFiles.Count)
+            {
+                reference.Output.Text = "Room files were added or removed, unequal match.";
+                matchFailed = true;
+                goto FAILED;
+            }
+
+            reference.Output.Text = $"{firstFiles.Count} room files detected.\n";
+
+            for(int i = 0; i < firstFiles.Count; i++)
+            {
+                if(provider.ComputeHash(File.OpenRead(firstFiles[i])).SequenceEqual(provider.ComputeHash(File.OpenRead(secondFiles[i]))))
+                    continue;
+
+                reference.Output.Text += $"Match failed for room {i}\n";
+                matchFailed = true;
+            }
+
+            FAILED:
+
+            if(!matchFailed)
+            {
+                reference.Output.Text += $"Match succeeded, all room files are equal.";
+            }
+        }
+    }
+}
